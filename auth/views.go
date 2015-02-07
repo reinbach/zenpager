0d57bb083fec06395d45640c503288894ab110f7
Greg Reinbach <greg@reinbach.com>
2015-02-07 08:21:48 -0500

    added basic form handling, with validation separated

--- auth/views.go
+++ auth/views.go
@@ -10,11 +10,26 @@ import (
 	"git.ironlabs.com/greg/zenpager/template"
 )
 
+type Fields []form.Field
+
+var (
+	Email = form.Field{
+		Name:       "email",
+		Required:   true,
+		Validators: form.Validators{form.Email{}},
+	}
+	Password = form.Field{
+		Name:     "password",
+		Required: true,
+	}
+)
+
 func Login(c web.C, w http.ResponseWriter, r *http.Request) {
 	ctx := template.NewContext()
 	if r.Method == "POST" {
 		if err := r.ParseForm(); err == nil {
-			v := form.Validate(r, []string{"email", "password"})
+			fields := Fields{Email, Password}
+			v := form.Validate(r, fields)
 			if v.Valid() == true {
 				// authenticate user
 				// check for next field and redirect to it
@@ -23,6 +38,7 @@ func Login(c web.C, w http.ResponseWriter, r *http.Request) {
 				session.SetCookie(w, r, USER_KEY, user)
 				http.Redirect(w, r, "/dashboard/", http.StatusFound)
 			} else {
+				ctx.Add("Form", v)
 				session.AddMessage(&c, w, r, "Form failed validation!")
 			}
 		} else {
--- form/fields.go
+++ form/fields.go
@@ -1,9 +1,40 @@
 package form
 
+import (
+	"fmt"
+	"net/http"
+)
+
 type Field struct {
-	Required string
+	Name       string
+	Required   bool
+	Validators []Validator
 }
 
-type FieldHandler interface {
-	Validate()
+type Validators []Validator
+
+type Validator interface {
+	Validate(*Field, string) (bool, string)
+}
+
+type Email struct{}
+
+func (e Email) Validate(f *Field, v string) (bool, string) {
+	if v[len(v)-3:] != ".com" {
+		return false, fmt.Sprintf("%v requires .com", f.Name)
+	}
+	return true, ""
+}
+
+func (f *Field) Validate(r *http.Request) (bool, string) {
+	v := r.PostFormValue(f.Name)
+	if f.Required == true && v == "" {
+		return false, fmt.Sprintf("%v is required", f.Name)
+	}
+	for _, validator := range f.Validators {
+		if valid, msg := validator.Validate(f, v); valid == false {
+			return valid, msg
+		}
+	}
+	return true, ""
 }
