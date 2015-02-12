03239dc727813bb9d8548850a1d28f47561e1c62
Greg Reinbach <greg@reinbach.com>
2015-02-12 08:19:21 -0500

    renamed encrypt functions

--- database/encrypt.go
+++ database/encrypt.go
@@ -4,7 +4,7 @@ import (
 	"golang.org/x/crypto/bcrypt"
 )
 
-func encrypt(s string) string {
+func Encrypt(s string) string {
 	b := []byte(s)
 	hashedString, err := bcrypt.GenerateFromPassword(b, 10)
 	if err != nil {
@@ -13,7 +13,7 @@ func encrypt(s string) string {
 	return string(hashedString)
 }
 
-func validate(hashedString, s string) bool {
+func Validate(hashedString, s string) bool {
 	err := bcrypt.CompareHashAndPassword([]byte(hashedString), []byte(s))
 	if err != nil {
 		return false
