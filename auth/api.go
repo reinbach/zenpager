87deccb5256a55850251a7c5262754857fc1efad
Greg Reinbach <greg@reinbach.com>
2015-03-24 16:52:17 -0400

    added user partial update endpoint

--- auth/api.go
+++ auth/api.go
@@ -4,12 +4,12 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/zenazn/goji/web"
 	"github.com/zenazn/goji/web/middleware"
 
 	"github.com/reinbach/zenpager/database"
-	"github.com/reinbach/zenpager/form"
 	"github.com/reinbach/zenpager/utils"
 )
 
@@ -26,6 +26,15 @@ func Routes() *web.Mux {
 	api.Post("/login", Login)
 	api.Get("/logout", Logout)
 
+	// user
+	// api.Get("/user/", UserList)
+	// api.Get("/user/:id", UserItem)
+	// api.Post("/user/", UserAdd)
+	// api.Put("/user/:id", UserUpdate)
+	api.Patch("/user/:id", UserPartialUpdate)
+	// api.Delete("/user/:id", UserDelete)
+	// api.Get("/user", http.RedirectHandler("/user/", 301))
+
 	return api
 }
 
@@ -38,7 +47,7 @@ func Login(c web.C, w http.ResponseWriter, r *http.Request) {
 		utils.EncodePayload(w, http.StatusBadRequest, res)
 		return
 	}
-	errors := user.Validate()
+	errors := user.Validate(true)
 	if len(errors) > 0 {
 		res = Response{Result: "error", Messages: errors}
 		utils.EncodePayload(w, http.StatusBadRequest, res)
@@ -66,4 +75,42 @@ func Logout(w http.ResponseWriter, r *http.Request) {
 		log.Println("Failed to encode response: ", err)
 	}
 	w.Write(b)
+}
+
+func UserPartialUpdate(c web.C, w http.ResponseWriter, r *http.Request) {
+	var db = database.FromContext(c)
+	var res = Response{}
+	id, err := strconv.ParseInt(c.URLParams["id"], 10, 64)
+	if err != nil {
+		res = Response{Result: "error", Messages: []string{"User not found."}}
+		utils.EncodePayload(w, http.StatusNotFound, res)
+		return
+	}
+	user := User{
+		ID: id,
+	}
+	if err := utils.DecodePayload(r, &user); err != nil {
+		res = Response{Result: "error", Messages: []string{"Data appears to be invalid."}}
+		utils.EncodePayload(w, http.StatusBadRequest, res)
+		return
+	}
+	user.Get(db)
+	if err := utils.DecodePayload(r, &user); err != nil {
+		res = Response{Result: "error", Messages: []string{"Data appears to be invalid."}}
+		utils.EncodePayload(w, http.StatusBadRequest, res)
+		return
+	}
+	errors := user.Validate(false)
+	if len(errors) > 0 {
+		res = Response{Result: "error", Messages: errors}
+		utils.EncodePayload(w, http.StatusBadRequest, res)
+		return
+	}
+	if user.Update(db) {
+		res = Response{Result: "success", Messages: []string{"User data updated."}}
+		utils.EncodePayload(w, http.StatusAccepted, res)
+	} else {
+		res = Response{Result: "error", Messages: []string{"Failed to update user."}}
+		utils.EncodePayload(w, http.StatusInternalServerError, res)
+	}
 }
--- auth/models.go
+++ auth/models.go
@@ -16,7 +16,7 @@ type User struct {
 
 func (u *User) Login(db *sql.DB) bool {
 	var password string
-	err := db.QueryRow("SELECT password FROM auth_user where email = $1",
+	err := db.QueryRow("SELECT password FROM auth_user WHERE email = $1",
 		u.Email).Scan(&password)
 	switch {
 	case err == sql.ErrNoRows:
@@ -38,7 +38,7 @@ func (u *User) Create(db *sql.DB) bool {
 	return true
 }
 
-func (u *User) Validate() []string {
+func (u *User) Validate(validate_password bool) []string {
 	var errors []string
 	if len(u.Email) < 1 {
 		errors = append(errors, "Email is required.")
@@ -47,8 +47,41 @@ func (u *User) Validate() []string {
 			errors = append(errors, "A valid Email address is required.")
 		}
 	}
-	if len(u.Password) < 1 {
-		errors = append(errors, "Password is required.")
+	if validate_password {
+		if len(u.Password) < 1 {
+			errors = append(errors, "Password is required.")
+		}
 	}
 	return errors
+}
+
+func (u *User) Get(db *sql.DB) {
+	err := db.QueryRow("SELECT email auth_user WHERE id = $1", u.ID).Scan(u.Email)
+	switch {
+	case err == sql.ErrNoRows:
+		log.Println("User not found.")
+	case err != nil:
+		log.Fatal(err)
+	}
+}
+
+func (u *User) Update(db *sql.DB) bool {
+	if len(u.Password) > 0 {
+		u.Password = database.Encrypt(u.Password)
+		_, err := db.Exec("UPDATE auth_user SET email = $1, password = $2 WHERE id = $3",
+			u.Email, u.Password, u.ID)
+		if err != nil {
+			log.Printf("Failed to update user record. ", err)
+			return false
+		}
+		return true
+	} else {
+		_, err := db.Exec("UPDATE auth_user SET email = $1 WHERE id = $2",
+			u.Email, u.ID)
+		if err != nil {
+			log.Printf("Failed to update user record. ", err)
+			return false
+		}
+		return true
+	}
 }
