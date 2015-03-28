3c444c03a14805384a828f84ab1618ebd15a8a22
Greg Reinbach <greg@reinbach.com>
2015-03-28 19:31:05 -0400

    page title set correctly, also drawer panel closes on selection when applicable

--- website/intro/app.js
+++ website/intro/app.js
@@ -8,4 +8,12 @@ document.addEventListener('polymer-ready', function() {
     template.addEventListener('template-bound', function(e) {
         this.route = this.route || DEFAULT_ROUTE;
     });
+
+    template.menuItemSelected = function(e, detail, sender) {
+        var title = document.getElementById("title");
+        title.innerText = detail.item.title;
+        if (detail.isSelected) {
+            document.querySelector('#scaffold').closeDrawer();
+        }
+    };
 });
