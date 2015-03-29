816b724a198c9ffeef62462976a6247dc9d3cae1
Greg Reinbach <greg@reinbach.com>
2015-03-29 16:24:55 -0400

    tweaked some styling for intro section

--- website/intro/app.js
+++ website/intro/app.js
@@ -2,9 +2,9 @@ document.addEventListener('polymer-ready', function() {
     var DEFAULT_ROUTE = "home";
     var template = document.querySelector('template[is="auto-binding"]');
     template.pages = [
-        {name: "Home", hash: "home"},
-        {name: "Contact", hash: "contact"},
-        {name: "Dashboard", hash: "dashboard"}
+        {name: "Home", hash: "home", icon: "home"},
+        {name: "Contact", hash: "contact", icon: "mail"},
+        {name: "Dashboard", hash: "dashboard", icon: "dashboard"}
     ];
     template.addEventListener('template-bound', function(e) {
         this.route = this.route || DEFAULT_ROUTE;
