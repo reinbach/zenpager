672b17dd881eacf694c04f004df38a5d2335436d
Greg Reinbach <greg@reinbach.com>
2015-03-30 20:07:59 -0400

    added submenu

--- website/src/app.js
+++ website/src/app.js
@@ -1,18 +1,30 @@
 document.addEventListener('polymer-ready', function() {
-    var DEFAULT_ROUTE = "dashboard";
+    var DEFAULT_ROUTE = "dashboard/overview";
     var template = document.querySelector('template[is="auto-binding"]');
-    template.pages = [
-        {name: "Dashboard", hash: "dashboard", icon: "dashboard"},
-        {name: "Settings", hash: "settings", icon: "settings"},
-        {name: "Profile", hash: "profile", icon: "profile"}
+    template.sections = [
+        {name: "Dashboard", hash: "dashboard", icon: "dashboard", pages: [
+            {name: "Overview", hash: "overview", icon: "overview"},
+            {name: "Servers", hash: "servers", icon: "servers"},
+            {name: "Applications", hash: "applications", icon: "applications"}
+        ]},
+        {name: "Settings", hash: "settings", icon: "settings", pages: [
+            {name: "Contacts", hash: "contacts", icon: "contacts"},
+            {name: "Servers", hash: "servers", icon: "servers"},
+            {name: "Time Periods", hash: "timeperiods", icon: "timeperiods"}
+        ]},
+        {name: "Profile", hash: "profile", icon: "account-box", pages: [
+            {name: "Reset Password", hash: "password", icon: "password"}
+        ]}
     ];
     template.addEventListener('template-bound', function(e) {
         this.route = this.route || DEFAULT_ROUTE;
     });
 
     template.menuItemSelected = function(e, detail, sender) {
-        var title = document.getElementById("title");
-        title.innerText = detail.item.title;
+        var sectionTitle = document.getElementById("sectionTitle");
+        var pageTitle = document.getElementById("pageTitle");
+        sectionTitle.innerText = detail.item.parentNode.label;
+        pageTitle.innerText = detail.item.title;
         if (detail.isSelected) {
             document.querySelector('#scaffold').closeDrawer();
         }
