6fdd85eeace9f849a00cde8b850c25be63830955
Greg Reinbach <greg@reinbach.com>
2015-04-07 20:10:15 -0400

    changed formatting of sql statements in auth initial file

--- auth/sql/0001_initial.sql
+++ auth/sql/0001_initial.sql
@@ -1,5 +1,5 @@
-create table auth_user (
-  id SERIAL,
-  email varchar(150) not null unique,
-  password varchar(150) not null
+CREATE TABLE auth_user (
+  id SERIAL UNIQUE,
+  email VARCHAR(150) NOT NULL UNIQUE,
+  password VARCHAR(150) NOT NULL
 );
