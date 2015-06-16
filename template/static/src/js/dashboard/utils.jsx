107763c072b53e7fae9edcb3e68aab6501f31028
Greg Reinbach <greg@reinbach.com>
2015-06-16 06:31:23 -0400

    added boilerplate ajax post method

--- template/static/src/js/dashboard/utils.jsx
+++ template/static/src/js/dashboard/utils.jsx
@@ -18,6 +18,25 @@ var request = {
         };
         r.send();
     },
+    post: function(url, data, cb) {
+        var r = new XMLHttpRequest();
+        r.open("POST", url, true);
+        r.setRequestHeader("Content-Type", "application/json");
+        r.setRequestHeader("X-Access-Token", auth.getToken());
+        r.onreadystatechange = function() {
+            if (r.readyState === 4) {
+                if (r.status == 401) {
+                    auth.logout();
+                } else {
+                    rdata = JSON.parse(r.responseText);
+                    if (cb) {
+                        cb(rdata);
+                    }
+                }
+            }
+        };
+        r.send(JSON.stringify(data));
+    },
     patch: function(url, data, cb) {
         var r = new XMLHttpRequest();
         r.open("PATCH", url, true);
