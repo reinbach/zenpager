941b5c5e10624713838ca936f83ebbd0e83f6d66
Greg Reinbach <greg@reinbach.com>
2015-06-16 06:13:57 -0400

    moved validation email function to validation file

--- template/static/src/js/dashboard/auth.jsx
+++ template/static/src/js/dashboard/auth.jsx
@@ -184,8 +184,3 @@ function logout(token) {
     };
     r.send();
 }
-
-function validateEmail(email) {
-    var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(email);
-}
