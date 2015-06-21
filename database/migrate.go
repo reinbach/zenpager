d549affba42e803d843076ed9e62e1d755efbc2b
Greg Reinbach <greg@reinbach.com>
2015-06-21 07:17:20 -0400

    migrations now check/handle required sql to be run prior to current sql file

--- database/migrate.go
+++ database/migrate.go
@@ -2,11 +2,14 @@ package database
 
 import (
 	"database/sql"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
 	"path/filepath"
+	"regexp"
+	"strings"
 
 	"github.com/reinbach/zenpager/utils"
 )
@@ -18,10 +21,11 @@ type Migration struct {
 }
 
 var (
-	d     = utils.GetAbsDir()
-	s     = map[string][]Migration{}
-	c     = map[string][]Migration{}
-	valid = true
+	d      = utils.GetAbsDir()
+	s      = map[string][]Migration{}
+	c      = map[string][]Migration{}
+	valid  = true
+	preSql = regexp.MustCompile(`require [a-z]+ [0-9]{4}_[a-z]+.sql`)
 )
 
 func MigrationExists(app, name string) bool {
@@ -61,31 +65,81 @@ func FindSqlFiles(path string, info os.FileInfo, err error) error {
 	return nil
 }
 
-func ExecSqlFiles(tx *sql.Tx, app string) error {
+func ExecSqlFiles(db *sql.DB, app string) error {
 	log.Printf("%s:\n", app)
 	for _, m := range s[app] {
-		if MigrationExists(app, m.Name) == false {
-			if f, err := ioutil.ReadFile(m.File); err == nil {
-				if err = ExecSql(tx, f, m); err != nil {
-					return err
-				}
-			} else {
-				log.Printf("Failed to get SQL from file %v: %v\n", m.File, err)
-				return err
-			}
+		if err := ExecSqlFile(db, app, m); err != nil {
+			return err
 		}
 	}
 	return nil
 }
 
-func ExecSql(tx *sql.Tx, f []byte, m Migration) error {
+func ExecSqlFile(db *sql.DB, app string, m Migration) error {
+	if MigrationExists(app, m.Name) == false {
+		if f, err := ioutil.ReadFile(m.File); err == nil {
+			// check if there is a 'requirement'
+			if err = ExecPreSqlFiles(db, f); err != nil {
+				log.Println("error in pre exec...")
+				return err
+			}
+			if err = ExecSql(db, f, m); err != nil {
+				return err
+			}
+			c = MigrationAdd(c, app, m.Name)
+		} else {
+			log.Printf("Failed to get SQL from file %v: %v\n", m.File, err)
+			return err
+		}
+	}
+	return nil
+}
+
+func ExecPreSqlFiles(db *sql.DB, f []byte) error {
+	// sql file for requirements
+	// if any requirements, parse out app and name of migration
+	// search migrations for match and then execute found match
+	p := preSql.FindAll(f, -1)
+	for _, n := range p {
+		split := strings.Split(fmt.Sprintf("%s", n), " ")
+		m, err := FindMigration(split[1], split[2])
+		if err != nil {
+			return err
+		}
+		if err = ExecSqlFile(db, split[1], m); err != nil {
+			log.Println("Failed to run requirement: %v (%v)", split[1], m)
+			return err
+		}
+	}
+	return nil
+}
+
+func FindMigration(app, name string) (Migration, error) {
+	for _, m := range s[app] {
+		if m.Name == name {
+			return m, nil
+		}
+	}
+	return Migration{}, errors.New("Requirement File Not Found")
+}
+
+func ExecSql(db *sql.DB, f []byte, m Migration) error {
+	tx, err := db.Begin()
+	if err != nil {
+		log.Println("Not able to start transaction: ", err)
+		os.Exit(1)
+	}
+
 	if _, err := tx.Exec(fmt.Sprintf("%s", f)); err != nil {
 		log.Printf("Failed to migrate %v: %v\n", m.File, err)
+		tx.Rollback()
 		return err
 	}
 	log.Printf("- %s\n", m.Name)
 	tx.Exec("INSERT INTO migrate (app, name, datetime) VALUES ($1, $2, NOW())",
 		m.App, m.Name)
+
+	tx.Commit()
 	return nil
 }
 
@@ -121,28 +175,15 @@ func Migrate() {
 		os.Exit(1)
 	}
 
-	tx, err := db.Begin()
-	if err != nil {
-		log.Println("Not able to start transaction: ", err)
-		os.Exit(1)
-	}
-
 	log.Println("Running migrations...\n")
+
 	// 'database' migrations run first
-	if err = ExecSqlFiles(tx, "database"); err != nil {
-		valid = false
-	} else {
+	if err := ExecSqlFiles(db, "database"); err == nil {
 		delete(s, "database")
 		// Run the rest of the migrations
 		for a, _ := range s {
-			ExecSqlFiles(tx, a)
+			ExecSqlFiles(db, a)
 		}
-	}
-
-	if valid == true {
-		tx.Commit()
-	} else {
-		tx.Rollback()
 	}
 
 	log.Println("\nFinished.\n")
