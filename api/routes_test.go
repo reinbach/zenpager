13f8b644e2421ce90a5daa2ecf72615e420f1449
Greg Reinbach <greg@reinbach.com>
2015-06-27 14:40:06 -0400

    added simple route calls to tests

--- api/routes_test.go
+++ api/routes_test.go
@@ -6,10 +6,20 @@ import (
 
 // Routes
 func TestRoutes(t *testing.T) {
+	Routes("/api/v1/")
+}
+
+// Auth Routes
+func TestAuthRoutes(t *testing.T) {
 	AuthRoutes()
 }
 
 // UserRoutes
 func TestUserRoutes(t *testing.T) {
 	UserRoutes()
+}
+
+// Contact Routes
+func TestContactRoutes(t *testing.T) {
+	ContactRoutes()
 }
